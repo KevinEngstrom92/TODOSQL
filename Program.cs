bc90ccda644b50afacc65ab3a2ecae7513f02ebd
Robert <robert.tublen@junction7.se>
2019-09-24 10:54:34 +0200

    Add ability to add todo

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using TODO.Domain;
 using static System.Console;
 
 namespace TODO
 {
     class Program
     {
+        static Task[] taskList = new Task[100];
+
         static void Main(string[] args)
         {
             bool shouldRun = true;
@@ -25,6 +28,16 @@ namespace TODO
                 {
                     case ConsoleKey.D1:
 
+                        Write("Title: ");
+
+                        string title = ReadLine();
+
+                        Write("Due date (yyyy-mm-dd hh:mm): ");
+
+                        DateTime dueDate = DateTime.Parse(ReadLine());
+
+                        taskList[GetIndexPosition()] = new Task(title, dueDate);
+
                         break;
 
                     case ConsoleKey.D3:
@@ -34,6 +47,28 @@ namespace TODO
                         break;
                 }
             }
+        }
+
+        static int GetIndexPosition()
+        {
+            int result = -1;
+            for (int i = 0; i < taskList.Length; i++)
+            {
+                if (taskList[i] != null)
+                {
+                    continue;
+                }
+                if (taskList[i] == null)
+                {
+                    result = i;
+                    break;
+                }
+                if (result == -1)
+                {
+                    throw new Exception("No avalible position");
+                }
+            }
+            return result;
         }
     }
 }
